839eb91f58e7b7f6adae46f9c00155d8075c4396
mdasif8393 <md.asif.8393@gmail.com>
2025-06-17 20:44:10 +0600

    17-6 Update and Delete a Note, Schema Options: timestamps, versionKey

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,55 +3,105 @@ import { model, Schema } from "mongoose";
 
 const app: Application = express();
 
+app.use(express.json());
+
 // create mongoose schema
-const noteSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  content: {
-    type: String,
-    default: "",
-  },
-  category: {
-    type: String,
-    enum: ["personal", "work", "study", "other"],
-    default: "personal",
-  },
-  pinned: {
-    type: Boolean,
-    default: false,
-  },
-  tags: {
-    label: {
+const noteSchema = new Schema(
+  {
+    title: {
       type: String,
       required: true,
+      trim: true,
     },
-    color: {
+    content: {
       type: String,
-      default: "gray",
+      default: "",
+    },
+    category: {
+      type: String,
+      enum: ["personal", "work", "study", "other"],
+      default: "personal",
+    },
+    pinned: {
+      type: Boolean,
+      default: false,
+    },
+    tags: {
+      label: {
+        type: String,
+        required: true,
+      },
+      color: {
+        type: String,
+        default: "gray",
+      },
     },
   },
-});
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
 
 // create mongoose model
 const Note = model("Note", noteSchema); // "Note" is model name
 
-app.post("/create-note", async (req: Request, res: Response) => {
-  const myNote = new Note({
-    title: "Learning Express",
-    tags: {
-      label: "database",
-    },
-  });
+app.post("/notes/create-note", async (req: Request, res: Response) => {
+  const body = req.body;
 
-  await myNote.save();
+  const note = await Note.create(body);
 
   res.status(201).json({
     status: true,
     message: "Note created successfully",
-    note: myNote,
+    note,
+  });
+});
+
+app.get("/notes", async (req: Request, res: Response) => {
+  const notes = await Note.find();
+
+  res.status(201).json({
+    status: true,
+    message: "Notes retrieved successfully",
+    notes,
+  });
+});
+
+app.get("/notes/:noteId", async (req: Request, res: Response) => {
+  const noteId = req.params.noteId;
+
+  const note = await Note.findById(noteId);
+
+  res.status(201).json({
+    status: true,
+    message: "Note retrieved successfully",
+    note,
+  });
+});
+
+app.patch("/notes/:noteId", async (req: Request, res: Response) => {
+  const noteId = req.params.noteId;
+  const updatedBody = req.body;
+
+  const note = await Note.findByIdAndUpdate(noteId, updatedBody, { new: true });
+
+  res.status(201).json({
+    status: true,
+    message: "Note updated successfully",
+    note,
+  });
+});
+
+app.delete("/notes/:noteId", async (req: Request, res: Response) => {
+  const noteId = req.params.noteId;
+
+  const note = await Note.findByIdAndDelete(noteId);
+
+  res.status(201).json({
+    status: true,
+    message: "Note deleted successfully",
+    note,
   });
 });
 
