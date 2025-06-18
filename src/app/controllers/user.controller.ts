3cd634387a98a8d35fd710281a4b6770ce589c8f
mdasif8393 <md.asif.8393@gmail.com>
2025-06-18 16:46:12 +0600

    18-3 Validate using Zod

--- src/app/controllers/user.controller.ts
+++ src/app/controllers/user.controller.ts
@@ -1,18 +1,35 @@
 import express, { Request, Response } from "express";
 import { User } from "../models/user.model";
+import { z } from "zod";
 
 export const usersRoutes = express.Router();
 
+const CreateUserZodSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  age: z.number(),
+  email: z.string(),
+  password: z.string(),
+  role: z.string().optional(),
+});
+
 usersRoutes.post("/create-user", async (req: Request, res: Response) => {
-  const body = req.body;
+  try {
+    const body = await CreateUserZodSchema.parseAsync(req.body);
+    const user = await User.create(body);
 
-  const user = await User.create(body);
-
-  res.status(201).json({
-    success: true,
-    message: "User created successfuly",
-    user,
-  });
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+      error,
+    });
+  }
 });
 usersRoutes.get("/", async (req: Request, res: Response) => {
   const users = await User.find();
