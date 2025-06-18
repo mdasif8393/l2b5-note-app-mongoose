0bd7cfbb90ca9891702cc539ff705fc579c6140f
mdasif8393 <md.asif.8393@gmail.com>
2025-06-18 14:26:53 +0600

    18-2 More About Built-in validation, Making Custom Validations & Third party Validator Package

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+import express, { Application, Request, Response } from "express";
 import { noteRoutes } from "./controllers/notes.controller";
 import { usersRoutes } from "./controllers/user.controller";
 
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,21 +1,42 @@
 import { model, Schema } from "mongoose";
 import { IUser } from "../interfaces/user.interface";
+import validator from "validator";
 
 const userSchema = new Schema<IUser>({
   firstName: {
     type: String,
-    required: true,
+    required: [true, "First Name is required"],
     trim: true,
+    minLength: [3, `First Name must be 3 character, got {value}`],
+    maxLength: 10,
   },
   lastName: {
     type: String,
     required: true,
     trim: true,
+    minLength: [3, `Last Name must be 3 character, got {value}`],
+    maxLength: 10,
+  },
+  age: {
+    type: Number,
+    required: true,
+    min: [18, `Minimum age must be 18 but got {VALUE}`],
+    max: 60,
   },
   email: {
     type: String,
     required: true,
+    unique: [true, "Email must be unique"],
     trim: true,
+    lowercase: true,
+    //// custom validation
+    // validate: {
+    //   validator: function (v) {
+    //     return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(v);
+    //   },
+    //   message: (props) => `${props.value} is not a valid email`,
+    // },
+    validate: [validator.isEmail, "{VALUE} is not a valid email"],
   },
   password: {
     type: String,
@@ -23,8 +44,12 @@ const userSchema = new Schema<IUser>({
   },
   role: {
     type: String,
-    enum: ["admin", "user"],
-    default: "user",
+    uppercase: true,
+    enum: {
+      values: ["ADMIN", "USER", "SUPERADMIN"],
+      message: `{value is not supported}`,
+    },
+    default: "USER",
   },
 });
 
